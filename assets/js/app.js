160ad1eb57fee34f64e5d3be357e9767e7e993ac
Qei <kei.yagi@gmail.com>
2018-08-22 14:38:52 +0900

    master: サンプルファイル追加

---
+++ assets/js/app.js
@@ -0,0 +1,114 @@
+(() => {
+  
+  /**
+   * WebRTCによるカメラアクセス
+   */
+  const video = document.getElementById('video')
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d')
+  
+  let isLoadedMetaData = false
+  let constraints = {audio: false, video: {facingMode: 'user'}}
+
+
+  function start(){
+    navigator.mediaDevices.getUserMedia( constraints )
+      .then( mediaStrmSuccess )
+      .catch( mediaStrmFailed )
+  }
+
+  function mediaStrmSuccess( stream ){
+    video.srcObject = stream
+
+    // ウェブカムのサイズを取得し、canvasにも適用
+    if(isLoadedMetaData) return
+    isLoadedMetaData = true
+
+    video.addEventListener('loadedmetadata', () => {
+      canvas.width = video.videoWidth      
+      canvas.height = video.videoHeight
+      requestAnimationFrame( draw )
+    }, false)
+  }
+
+  function mediaStrmFailed( e ){
+    console.log( e )
+  }
+
+  function stop(){
+    let stream = video.srcObject
+    let tracks = stream.getTracks()
+
+    tracks.forEach( (track) => {
+      track.stop()
+    })
+    video.srcObject = null
+  }
+
+  function draw(){
+    scanCharacter()
+    requestAnimationFrame( draw )
+  }
+
+  start()
+
+
+  /**
+   * ストリームのコントロール
+   */
+  const stopBtn = document.getElementById('stop')
+  const frontBtn = document.getElementById('front')
+  const rearBtn = document.getElementById('rear')
+  let isRun = false
+
+  let ua = navigator.userAgent
+  if(ua.indexOf('iPhone') < 0 && ua.indexOf('Android') < 0 && ua.indexOf('Mobile') < 0 && ua.indexOf('iPad') < 0){
+    frontBtn.disabled = true
+    rearBtn.disabled = true
+  }
+
+  stopBtn.addEventListener('click', () => {
+    if(!isRun){
+      stop()
+      stopBtn.textContent = 'START'
+    }else{
+      start()
+      stopBtn.textContent = 'STOP'
+    }
+    isRun = !isRun
+  }, false)
+
+  frontBtn.addEventListener('click', () => {
+    stop()
+    constraints.video.facingMode = 'user'
+    setTimeout( () => {
+      start()
+    }, 500)
+  }, false)
+
+  rearBtn.addEventListener('click', () => {
+    stop()
+    constraints.video.facingMode = 'environment'
+    setTimeout( () => {
+      start()
+    }, 500)
+  }, false)
+
+
+  /**
+   * 文字スキャン
+   */
+  const outputTxt = document.querySelector('.output_txt')
+
+  function scanCharacter(){
+    ctx.drawImage( video, 0, 0 )
+
+    Tesseract.recognize( ctx, {lang: 'eng'} )
+      .then( (result) => {
+        console.log(result)
+        outputTxt.textContent = result.text
+      })
+  }
+
+
+})()
